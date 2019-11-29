0c794b700b58a1bfc1a60b00d0429b722b5c489e
co0ontty <shicq@knownsec.com>
2019-11-29 18:33:22 +0800

    删除注释

--- Mario.py
+++ Mario.py
@@ -1,9 +1,7 @@
 import os
-import lib.data
 import api.ip
-#print (os.getcwd())
 dir=os.environ.get(os.getcwd())
 print (api.ip.ipAnalysis.get_city('95.169.17.220'))
-# print (api.ip.ipAnalysis.get_city('95.169.17.220',language='zh-CN'))
-print (api.ip.ipAnalysis.get_location('95.169.17.220'))
+# print (api.ip.ipAnalysis.get_city('95.169.17.220',language='zh-CN',location=True))
+
 
--- api/ip.py
+++ api/ip.py
@@ -1,25 +1,16 @@
-# import geoip2.database
-# GeoipDatabase = geoip2.database.Reader('GeoLite2-City.mmdb')
-# response_data = GeoipDatabase.city('97.132.182.78')
-# # 获取经纬度
-# longitude = response_data.location.longitude
-# latitude = response_data.location.latitude
-# # 城市名称
-# cityname = response_data.city.name
-# # 国家名称
-# country = response_data.country.name
-# print (country,cityname,longitude,latitude)
 
-# GeoipDatabase.close()
 import geoip2.database
 GeoipDatabase = geoip2.database.Reader('files/GeoLite2-City.mmdb')
 class ipAnalysis(object):
-    def get_city(ip,language = "en"):
+    def get_city(ip,language = "en",location = False):
         response_data = GeoipDatabase.city(ip)
-        try:
-            return response_data.country.names[language],response_data.city.names[language]
-        except :
-            return response_data.country.names['en'],response_data.city.names['en']
-    def get_location(ip):
-        response_data = GeoipDatabase.city(ip)
-        return response_data.location.longitude,response_data.location.latitude
+        if location:
+            try:
+                return response_data.country.names[language],response_data.city.names[language],response_data.location.longitude,response_data.location.latitude
+            except :
+                return response_data.country.names['en'],response_data.city.names['en'],response_data.location.longitude,response_data.location.latitude
+        else:
+            try:
+                return response_data.country.names[language],response_data.city.names[language]
+            except :
+                return response_data.country.names['en'],response_data.city.names['en']
