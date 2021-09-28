cfa4630e20d9ca5989ded173bb2a9cb05c9ab61d
Administrator <admin@example.com>
2021-09-28 10:28:30 +0800

    many 修改

--- run.py
+++ run.py
@@ -214,4 +214,4 @@ if __name__ == '__main__':
     start()
     app.config['JSON_AS_ASCII'] = False
     app.config['JSON_SORT_KEYS'] = False
-    app.run(host='172.16.0.1',port=5000, debug=True)
+    app.run(host='127.0.0.1',port=5000, debug=True)
